04f38d99c17e200546e335a9edfd6537ba51211c
Iryna Kalanchuk <irynakalanchuk@Irynas-MacBook-Pro.local>
2020-03-09 14:25:54 +0200

    added homework about Employees

--- src/ContractedEmployee.java
+++ src/ContractedEmployee.java
@@ -1,35 +1,48 @@
-public class ContractedEmployee {
+public class ContractedEmployee extends Employee{
+  private String federalTaxId;
+  private double numberOfHoursWorked;
+  private double hourlyRate;
   // TODO fix class declaration and declare variables here
 
   public ContractedEmployee(String employeeId, String name, String federalTaxId) {
+    super(employeeId, name);
+    this.federalTaxId = federalTaxId;
     // TODO fill in code here
   }
 
   public String getFederalTaxId() {
     // TODO fill in code here and replace the return statement
-    return "";
+    return federalTaxId;
   }
 
   public void setFederalTaxId(String federalTaxId) {
     // TODO fill in code here
+    this.federalTaxId = federalTaxId;
   }
 
   public double getHourlyRate() {
     // TODO fill in code here and replace the return statement
-    return 0;
+    return hourlyRate;
   }
 
   public void setHourlyRate(double hourlyRate) {
     // TODO fill in code here
+    this.hourlyRate = hourlyRate;
   }
 
   public double getNumberOfHoursWorked() {
     // TODO fill in code here and replace the return statement
-    return 0;
+    return numberOfHoursWorked;
   }
 
   public void setNumberOfHoursWorked(double numberOfHoursWorked) {
     // TODO fill in code here
+    this.numberOfHoursWorked = numberOfHoursWorked;
+  }
+
+  @Override
+  public void calculatePay() {
+    avgMonthlySalary = hourlyRate * numberOfHoursWorked;
   }
 
   // TODO fill in code here
--- src/SalariedEmployee.java
+++ src/SalariedEmployee.java
@@ -1,26 +1,37 @@
-public class SalariedEmployee {
+public class SalariedEmployee extends Employee{
+  private String socialSecurityNumber;
+  private double fixedMonthlyPayment;
   // TODO fix class declaration and declare variables here
 
   public SalariedEmployee(String employeeId, String name, String socialSecurityNumber) {
+    super(employeeId, name);
+    this.socialSecurityNumber = socialSecurityNumber;
     // TODO fill in code here
   }
 
   public String getSocialSecurityNumber() {
     // TODO fill in code here and replace the return statement
-    return "";
+    return socialSecurityNumber;
   }
 
   public void setSocialSecurityNumber(String socialSecurityNumber) {
     // TODO fill in code here
+    this.socialSecurityNumber = socialSecurityNumber;
   }
 
   public double getFixedMonthlyPayment() {
     // TODO fill in code here and replace the return statement
-    return 0;
+    return fixedMonthlyPayment;
   }
 
   public void setFixedMonthlyPayment(double fixedMonthlyPayment) {
     // TODO fill in code here
+    this.fixedMonthlyPayment = fixedMonthlyPayment;
+  }
+
+  @Override
+  public void calculatePay() {
+    avgMonthlySalary = fixedMonthlyPayment;
   }
 
   // TODO fill in code here
